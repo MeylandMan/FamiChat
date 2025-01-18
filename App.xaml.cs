b69179b8dc0dca78715a355dd79c13e23356a411
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-01-18 18:49:32 +0100

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace discord_like
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
