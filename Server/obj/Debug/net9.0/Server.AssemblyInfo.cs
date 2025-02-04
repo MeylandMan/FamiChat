f29755fada830780fc79874852362efa1dbacfe6
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-04 22:22:32 +0100

    Setup both Sever and Shared projects

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3ab33b2631b090aea616307e4bec2df8056a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088e186837b4bc0b54628747565c595cf57b0f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3ab33b2631b090aea616307e4bec2df8056a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088e186837b4bc0b54628747565c595cf57b0f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
